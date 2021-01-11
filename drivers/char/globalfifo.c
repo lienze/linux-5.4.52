1202f0845cdd66870e2d489cacc1deda6b3f1688
Enze Li <lienze2010@hotmail.com>
2021-01-11 12:45:37 +0800

    add poll support
    Signed-off-by: Enze Li <lienze2010@hotmail.com>

--- drivers/char/globalfifo.c
+++ drivers/char/globalfifo.c
@@ -5,6 +5,7 @@
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 #include <linux/sched/signal.h>
+#include <linux/poll.h>
 
 #define GLOBALFIFO_SIZE			0x1000
 #define MEM_CLEAR			0x1
@@ -189,6 +190,28 @@ static loff_t globalfifo_llseek(struct file *filp, loff_t offset, int orig)
 	return ret;
 }
 
+static unsigned int globalfifo_poll(struct file *filp, poll_table *wait)
+{
+	unsigned int mask = 0;
+	struct globalfifo_dev *dev = filp->private_data;
+
+	mutex_lock(&dev->mutex);
+
+	poll_wait(filp, &dev->r_wait, wait);
+	poll_wait(filp, &dev->w_wait, wait);
+
+	if (dev->current_len != 0) {
+		mask |= POLLIN | POLLRDNORM;
+	}
+
+	if (dev->current_len != GLOBALFIFO_SIZE) {
+		mask |= POLLOUT | POLLWRNORM;
+	}
+
+	mutex_unlock(&dev->mutex);
+	return mask;
+}
+
 static const struct file_operations globalfifo_fops = {
 	.owner = THIS_MODULE,
 	.llseek = globalfifo_llseek,
@@ -197,6 +220,7 @@ static const struct file_operations globalfifo_fops = {
 	.unlocked_ioctl = globalfifo_ioctl,
 	.open = globalfifo_open,
 	.release = globalfifo_release,
+	.poll = globalfifo_poll,
 };
 
 static void globalfifo_setup_cdev(struct globalfifo_dev *dev, int index)
